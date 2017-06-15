de7e0390448df24b343e728dda9e7aa12b07739d
dendrite <dendrite.sysex@gmail.com>
2017-06-15 10:51:05 -0400

    Crank up pylint score to 10.0

--- src/pysex/cell.py
+++ src/pysex/cell.py
@@ -10,7 +10,7 @@
       ListCell:
           for value enumerations, evaluates to a list
       SubstCell:
-          for '(...)' substitutions    
+          for '(...)' substitutions
 
 '''
 
@@ -24,14 +24,11 @@ def encast(val, number=False, base=10):
         - return: val or val cast to float, int, False, True or None
     '''
     try:
-        if isinstance(val, str):
-            return int(val, base)
+        return int(val, base)
     except ValueError:
         pass
 
     try:
-        if not isinstance(val, str):
-            raise ValueError()
         return float(val)
     except ValueError:
         if number:
@@ -48,19 +45,20 @@ def encast(val, number=False, base=10):
         }[val]
     except KeyError:
         return val
-    except TypeError:
-        return val
+
 
 class CellIterator(object):
     ''' iterator class for parsing cell tokens
     '''
+    # pylint: disable=too-few-public-methods
+
     def __init__(self, loc, buf):
         self._loc = loc
         self._buf = buf
 
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         ''' yield next argument of a substitution
             - loc: [f, r, c] of cell
@@ -71,7 +69,7 @@ class CellIterator(object):
         self._buf = self._buf.lstrip()
         if self._buf.startswith(')'):
             self._buf = self._buf[1:]
-    
+
         if len(self._buf) == 0:
             raise StopIteration
 
@@ -82,6 +80,7 @@ class CellIterator(object):
         result = self._buf[mat.start():mat.end()]
         self._buf = self._buf[mat.end():]
         return result
+
 
 class Cell(object):
     ''' A Cell in our model
@@ -131,7 +130,10 @@ class Cell(object):
             - values: list of components of the cell
         '''
         self._loc = loc
-        self._value = encast(buf)
+        if isinstance(buf, str):
+            self._value = encast(buf)
+        else:
+            self._value = buf
 
     def __str__(self):
         return '( loc: %s, value: %s )' % (self._loc, self._value)
@@ -171,7 +173,7 @@ class RangeCell(Cell):
     '''
     def __init__(self, loc, buf):
         super(RangeCell, self).__init__(loc, buf)
-    
+
     def value(self, context, arg=None):
         lef, rig = self._value
         if isinstance(lef, Cell):
@@ -212,11 +214,12 @@ class SubstCell(Cell):
         return buf, subclass(loc, buf.lstrip())
 
     def __init__(self, loc, buf, number=False, base=10):
+        # pylint: disable=no-member,unused-argument
         super(SubstCell, self).__init__(loc, [])
 
         def _fail(xxx, yyy):
             raise ValueError('bad SubstCell._op override')
-        
+
         for val in CellIterator(loc, buf):
             if isinstance(val, str):
                 try:
@@ -235,13 +238,16 @@ class SubstCell(Cell):
         ''' evaluate. self._value is always an array of at least 2 items
               pass values pairwise to self._op and accumulate results
         '''
+        # pylint: disable=no-member
         result = self._value.pop(0)
         if isinstance(result, Cell):
             result = result.value(context, arg)
-            
+
         for val in self._value:
             if isinstance(val, Cell):
                 val = val.value(context, arg)
+
+            # pylint: disable=assignment-from-no-return
             result = self._op(result, val)
 
         return result
@@ -251,6 +257,8 @@ class AddCell(SubstCell):
     ''' implement (+ v v ...) substitution
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #  thinks ._value is int/float
         super(AddCell, self).__init__(self, loc, buf, number=True)
         self._value.insert(0, 0)
         self._op = lambda x, y: x + y
@@ -259,6 +267,8 @@ class AndCell(SubstCell):
     ''' implement (& v v ...)
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #  thinks ._value is int/float
         super(AndCell, self).__init__(self, loc, buf, number=True)
         self._value.insert(0, 0xFFFFFFFF)
         self._op = lambda x, y: x & y
@@ -268,11 +278,15 @@ class HexCell(SubstCell):
     ''' implement (# vv ...)
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #  thinks ._value is int/float
         super(HexCell, self).__init__(loc, buf, number=True, base=16)
         self._value.insert(0, [])
         self._op = lambda x, y: x + [y]
 
     def value(self, context, arg=None):
+        # pylint: disable=no-member
+        #  thinks ._value is int/float
         val = super(HexCell, self).value(context, arg)
         if len(val) == 1:
             return val.pop()
@@ -283,6 +297,8 @@ class NotCell(SubstCell):
     ''' implement (~ v)
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #  thinks ._value is int/float
         super(NotCell, self).__init__(self, loc, buf, number=True)
         self._value.insert(0, 0)
         self._op = lambda x, y: ~y
@@ -292,6 +308,8 @@ class OrCell(SubstCell):
     ''' implement (| v v ...) substitution
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #  thinks ._value is int/float
         super(OrCell, self).__init__(self, loc, buf, number=True)
         self._value.insert(0, 0x00000000)
         self._op = lambda x, y: x | y
@@ -303,17 +321,19 @@ class PasteCell(SubstCell):
     def __init__(self, loc, buf):
         ''' ctor
         '''
-        def paste(sss, ttt):
+        # pylint: disable=no-member
+        def paste(s, t):
             ''' contatenate tokens, processing % and %% metachars
-                - sss: left side
-                - ttt: right side
+                - s: left side
+                - t: right side
             '''
-            if ttt == '%':
-                ttt = ' '
-            elif ttt == '%%':
-                ttt = '%'
+            # pylint: disable=invalid-name
+            if t == '%':
+                t = ' '
+            elif t == '%%':
+                t = '%'
 
-            return '%s%s' % (sss, ttt)
+            return '%s%s' % (s, t)
 
         super(PasteCell, self).__init__(self, loc, buf)
         self._value.insert(0, '')
@@ -346,6 +366,8 @@ class SubCell(SubstCell):
     ''' implement (- v v ...)
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #   thinks self._value is int/float
         super(SubCell, self).__init__(self, loc, buf, number=True)
         if len(self._value) == 1:
             self._value.insert(0, 0)
@@ -356,6 +378,8 @@ class VarCell(SubstCell):
     ''' class for (* * *)
     '''
     def __init__(self, loc, buf):
+        # pylint: disable=no-member
+        #  it thinks self._value is int or float
         if buf.startswith(']'):
             scope = 't'
             buf = buf[1:]
@@ -374,8 +398,10 @@ class VarCell(SubstCell):
 
 
 def units():
+    ''' run unit tests
+    '''
     context = {}
-    
+
     test = 0
     cell = Cell.factory(['string', 0, test], "hello")
     print('%s\n: %s\n' % (cell, cell.value(context)))
@@ -404,7 +430,7 @@ def units():
         print('%s\n: %s\n' % (cell, cell.value(context)))
     except ValueError as err:
         print('! caught expected ValueError:\n!   %s\n' % err)
-    
+
     test += 1
     cell = Cell.factory( ['(#80)', 0, test], "(#80)")
     print('%s\n: %s\n' % (cell, cell.value(context)))
